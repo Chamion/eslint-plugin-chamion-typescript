a57841b9568283bcacc8359ee35d1777a4aba2c7
Jemi Salo <jemi.salo@gmail.com>
2022-08-31 16:28:50 +0300

    Add prefer-number-isnan

---
+++ src/rules/prefer-number-isnan.js
@@ -0,0 +1,44 @@
+const createPreferNumberIsnan = (context) => ({
+  BinaryExpression: (node) => {
+    if (
+      node.left.type !== "Identifier" ||
+      node.right.type !== "Identifier" ||
+      node.left.name !== node.right.name
+    )
+      return;
+    let complement;
+    if (node.operator === "===" || node.operator === "==") {
+      complement = true;
+    } else if (node.operator === "!==" || node.operator === "!=") {
+      complement = false;
+    } else return;
+    const source = context.getSourceCode();
+    const commentsInside = source.getCommentsInside(node);
+    const fix =
+      commentsInside.length > 0
+        ? undefined
+        : (fixer) =>
+            fixer.replaceText(
+              node,
+              `${complement ? "!" : ""}Number.isNaN(${node.left.name})`
+            );
+    context.report({
+      node,
+      message:
+        "Do not compare a variable to itself. Use Number.isNaN to check for NaN.",
+      fix,
+    });
+  },
+});
+
+export default {
+  meta: {
+    type: "suggestion",
+    docs: {
+      description: "Disallows checking for NaN by self comparison",
+      suggestion: true,
+    },
+    fixable: "code",
+  },
+  create: createPreferNumberIsnan,
+};
