727826f0aaf8e944434ed5374c48162ff3880ab2
Jemi Salo <jemi.salo@gmail.com>
2022-08-31 15:53:10 +0300

    Configure no-unique-object-comparison

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import preferPromiseRejection from "./rules/prefer-promise-rejection";
 import explicitVoidReturn from "./rules/explicit-void-return";
 import noReturnIntoVoid from "./rules/no-return-into-void";
+import noUniqueObjectComparison from "./rules/no-unique-object-comparison";
 
 module.exports = {
   rules: {
     "prefer-promise-rejection": preferPromiseRejection,
     "explicit-void-return": explicitVoidReturn,
     "no-return-into-void": noReturnIntoVoid,
+    "no-unique-object-comparison": noUniqueObjectComparison,
   },
   configs: {
     recommended: {
@@ -14,6 +16,7 @@ module.exports = {
       rules: {
         "chamion-typescript/prefer-promise-rejection": "error",
         "chamion-typescript/no-return-into-void": "error",
+        "chamion-typescript/no-unique-object-comparison": "error",
       },
     },
   },
