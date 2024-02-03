5097f3ade0e6eb52178ddb2e72524dffa992a70d
Jemi Salo <jemi.salo@gmail.com>
2024-02-03 12:16:21 +0200

    Replace token comparison with recursive heuristics

--- src/rules/narrowing-type-predicates.ts
+++ src/rules/narrowing-type-predicates.ts
@@ -64,21 +64,240 @@ export default createRule({
   },
   defaultOptions: [],
   create: (context) => {
-    const { getText, getTokens, scopeManager } = context.getSourceCode();
+    const { getText, scopeManager } = context.getSourceCode();
     if (scopeManager == null) return {};
     type Scope = NonNullable<ReturnType<(typeof scopeManager)["acquire"]>>;
 
-    const tokenEquals = (a: TSESTree.Node, b: TSESTree.Node) => {
-      const aTokens = getTokens(a);
-      const bTokens = getTokens(b);
-      return (
-        aTokens.length === bTokens.length &&
-        aTokens.every(
-          (_, index) =>
-            aTokens[index].value === bTokens[index].value &&
-            aTokens[index].type === bTokens[index].type
-        )
+    const arrayElementsEqual =
+      <T>(equals: (a: T, b: T) => boolean) =>
+      (head: readonly T[], ...rest: readonly (readonly T[])[]) =>
+        rest.every(
+          (list) =>
+            list.length === head.length &&
+            list.every((element, index) => equals(element, head[index]))
+        );
+
+    const templateElementsEqual = (
+      a: TSESTree.TemplateElement,
+      b: TSESTree.TemplateElement
+    ) => a.value.raw === b.value.raw && a.value.cooked === b.value.cooked;
+
+    const parameterNamesEqual = (
+      head: TSESTree.Identifier | TSESTree.TSThisType,
+      ...rest: readonly (TSESTree.Identifier | TSESTree.TSThisType)[]
+    ) =>
+      head.type === AST_NODE_TYPES.TSThisType
+        ? rest.every((parameterName) => parameterName.type === head.type)
+        : rest.every(isIdentifier(head.name));
+
+    const typeParametersEqual = (
+      head: TSESTree.TSTypeParameter,
+      ...rest: readonly TSESTree.TSTypeParameter[]
+    ) =>
+      rest.every(
+        (typeParameter) =>
+          typeParameter.name.name === head.name.name &&
+          typeParameter.in === head.in &&
+          typeParameter.out === head.out &&
+          nullableEquals(typesEqual)(head.constraint, typeParameter.constraint)
       );
+
+    const parametersEqual = (
+      head: TSESTree.Parameter,
+      ...rest: readonly TSESTree.Parameter[]
+    ) => {
+      switch (head.type) {
+        case AST_NODE_TYPES.Identifier:
+          return rest.every(
+            (parameter) =>
+              parameter.type === head.type &&
+              parameter.name === head.name &&
+              nullableEquals(typesEqual)(
+                head.typeAnnotation?.typeAnnotation,
+                parameter.typeAnnotation?.typeAnnotation
+              )
+          );
+        case AST_NODE_TYPES.RestElement:
+          return rest.every(
+            (parameter) =>
+              parameter.type === head.type &&
+              nullableEquals(typesEqual)(
+                head.typeAnnotation?.typeAnnotation,
+                parameter.typeAnnotation?.typeAnnotation
+              ) // TODO: further comparison
+          );
+        default:
+          // These cases should not happen in a type expression
+          return true;
+      }
+    };
+
+    const nullableEquals =
+      <T>(equals: (a: T, b: T) => boolean) =>
+      (
+        head: T | null | undefined,
+        ...rest: readonly (T | null | undefined)[]
+      ) =>
+        head == null
+          ? rest.every((element) => element == null)
+          : rest.every((element) => element != null && equals(head, element));
+
+    const typesEqual = (
+      a: TSESTree.TypeNode,
+      b: TSESTree.TypeNode
+    ): boolean => {
+      switch (a.type) {
+        case AST_NODE_TYPES.TSArrayType: {
+          const castB = b as any as typeof a;
+          return typesEqual(a.elementType, castB.elementType);
+        }
+        case AST_NODE_TYPES.TSMappedType: {
+          const castB = b as any as typeof a;
+          return (
+            typeParametersEqual(a.typeParameter, castB.typeParameter) &&
+            nullableEquals(typesEqual)(a.nameType, castB.nameType) &&
+            a.optional === castB.optional &&
+            a.readonly === castB.readonly &&
+            nullableEquals(typesEqual)(a.typeAnnotation, castB.typeAnnotation)
+          );
+        }
+        case AST_NODE_TYPES.TSOptionalType:
+        case AST_NODE_TYPES.TSRestType: {
+          const castB = b as any as typeof a;
+          return typesEqual(a.typeAnnotation, castB.typeAnnotation);
+        }
+        case AST_NODE_TYPES.TSTypeOperator: {
+          const castB = b as any as typeof a;
+          return (
+            a.operator === castB.operator &&
+            nullableEquals(typesEqual)(a.typeAnnotation, castB.typeAnnotation)
+          );
+        }
+        case AST_NODE_TYPES.TSTypePredicate: {
+          const castB = b as any as typeof a;
+          return (
+            parameterNamesEqual(a.parameterName, castB.parameterName) &&
+            a.asserts === castB.asserts &&
+            nullableEquals(typesEqual)(
+              a.typeAnnotation?.typeAnnotation,
+              castB.typeAnnotation?.typeAnnotation
+            )
+          );
+        }
+        case AST_NODE_TYPES.TSTemplateLiteralType: {
+          const castB = b as any as typeof a;
+          return (
+            arrayElementsEqual(templateElementsEqual)(a.quasis, castB.quasis) &&
+            arrayElementsEqual(typesEqual)(a.types, castB.types)
+          );
+        }
+        case AST_NODE_TYPES.TSUnionType:
+        case AST_NODE_TYPES.TSIntersectionType: {
+          const castB = b as any as typeof a;
+          return arrayElementsEqual(typesEqual)(a.types, castB.types);
+        }
+        case AST_NODE_TYPES.TSTupleType: {
+          const castB = b as any as typeof a;
+          return arrayElementsEqual(typesEqual)(
+            a.elementTypes,
+            castB.elementTypes
+          );
+        }
+        case AST_NODE_TYPES.TSConditionalType: {
+          const castB = b as any as typeof a;
+          return (
+            typesEqual(a.checkType, castB.checkType) &&
+            typesEqual(a.extendsType, castB.extendsType) &&
+            typesEqual(a.falseType, castB.falseType) &&
+            typesEqual(a.trueType, castB.trueType)
+          );
+        }
+        case AST_NODE_TYPES.TSFunctionType: {
+          const castB = b as any as typeof a;
+          return (
+            nullableEquals(typesEqual)(
+              a.returnType?.typeAnnotation,
+              castB.returnType?.typeAnnotation
+            ) &&
+            nullableEquals(arrayElementsEqual(typeParametersEqual))(
+              a.typeParameters?.params,
+              castB.typeParameters?.params
+            ) &&
+            arrayElementsEqual(parametersEqual)(a.params, castB.params)
+          );
+        }
+        case AST_NODE_TYPES.TSInferType: {
+          const castB = b as any as typeof a;
+          return typeParametersEqual(a.typeParameter, castB.typeParameter);
+        }
+        case AST_NODE_TYPES.TSTypeReference: {
+          const castB = b as any as typeof a;
+          return nullableEquals(arrayElementsEqual(typesEqual))(
+            a.typeParameters?.params,
+            castB.typeParameters?.params
+          );
+        }
+        case AST_NODE_TYPES.TSNamedTupleMember: {
+          const castB = b as any as typeof a;
+          return (
+            a.label.name === castB.label.name &&
+            typesEqual(a.elementType, castB.elementType)
+          );
+        }
+        case AST_NODE_TYPES.TSIndexedAccessType: {
+          const castB = b as any as typeof a;
+          return (
+            typesEqual(a.indexType, castB.indexType) &&
+            typesEqual(a.objectType, castB.objectType)
+          );
+        }
+        case AST_NODE_TYPES.TSConstructorType: {
+          const castB = b as any as typeof a;
+          return (
+            a.abstract === castB.abstract &&
+            nullableEquals(typesEqual)(
+              a.returnType?.typeAnnotation,
+              castB.returnType?.typeAnnotation
+            ) &&
+            nullableEquals(arrayElementsEqual(typeParametersEqual))(
+              a.typeParameters?.params,
+              castB.typeParameters?.params
+            )
+          );
+        }
+        case AST_NODE_TYPES.TSAbstractKeyword:
+        case AST_NODE_TYPES.TSAnyKeyword:
+        case AST_NODE_TYPES.TSAsyncKeyword:
+        case AST_NODE_TYPES.TSBigIntKeyword:
+        case AST_NODE_TYPES.TSBooleanKeyword:
+        case AST_NODE_TYPES.TSExportKeyword:
+        case AST_NODE_TYPES.TSDeclareKeyword:
+        case AST_NODE_TYPES.TSIntrinsicKeyword:
+        case AST_NODE_TYPES.TSNeverKeyword:
+        case AST_NODE_TYPES.TSNullKeyword:
+        case AST_NODE_TYPES.TSNumberKeyword:
+        case AST_NODE_TYPES.TSObjectKeyword:
+        case AST_NODE_TYPES.TSPrivateKeyword:
+        case AST_NODE_TYPES.TSProtectedKeyword:
+        case AST_NODE_TYPES.TSPublicKeyword:
+        case AST_NODE_TYPES.TSReadonlyKeyword:
+        case AST_NODE_TYPES.TSStaticKeyword:
+        case AST_NODE_TYPES.TSStringKeyword:
+        case AST_NODE_TYPES.TSSymbolKeyword:
+        case AST_NODE_TYPES.TSThisType:
+        case AST_NODE_TYPES.TSUndefinedKeyword:
+        case AST_NODE_TYPES.TSUnknownKeyword:
+        case AST_NODE_TYPES.TSVoidKeyword:
+          return true;
+        case AST_NODE_TYPES.TSImportType:
+        case AST_NODE_TYPES.TSQualifiedName:
+        case AST_NODE_TYPES.TSTypeLiteral:
+        case AST_NODE_TYPES.TSTypeQuery:
+        case AST_NODE_TYPES.TSLiteralType:
+        default:
+          // Default to not report a problem if comparison is not implemented
+          return true;
+      }
     };
 
     const isNarrowingReturnExpression = (
@@ -185,7 +404,7 @@ export default createRule({
       narrowingReturnLeaves.forEach(({ expressions: [satisfies, literal] }) => {
         const satisfiesType = satisfies.typeAnnotation;
         if (literal.value) {
-          if (!tokenEquals(satisfiesType, isType.typeAnnotation)) {
+          if (!typesEqual(satisfiesType, isType.typeAnnotation)) {
             context.report({
               node: satisfiesType,
               messageId: "return",
@@ -198,7 +417,7 @@ export default createRule({
           }
         } else if (isExcludeExpression(satisfiesType)) {
           const [excludedFrom, exclusion] = satisfiesType.typeParameters.params;
-          if (!tokenEquals(excludedFrom, paramType.typeAnnotation)) {
+          if (!typesEqual(excludedFrom, paramType.typeAnnotation)) {
             context.report({
               node: excludedFrom,
               messageId: "param",
@@ -209,7 +428,7 @@ export default createRule({
                 ),
             });
           }
-          if (!tokenEquals(exclusion, isType.typeAnnotation)) {
+          if (!typesEqual(exclusion, isType.typeAnnotation)) {
             context.report({
               node: exclusion,
               messageId: "return",
